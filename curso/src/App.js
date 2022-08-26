3b6e69476501199f5466baafe19287524fc92b33
Alexander Fonseca <101671694+Alexander-Fonseca@users.noreply.github.com>
2022-08-26 11:54:52 -0300

    Initial commit

---
+++ curso/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import TextCounter from './TextCounter';
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <TextCounter/>
+      </div>
+    )
+  }
+  
+}
+
+export default App;
